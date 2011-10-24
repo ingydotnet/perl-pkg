ae4d812724e3c14c5ac51b1dad631a439b0a3290
Ingy dot Net <ingy@ingy.net>
2011-10-24 12:06:06 -0700

    Make default module more interesting.

--- lib/%module.path%.pm
+++ lib/%module.path%.pm
@@ -1,3 +1,39 @@
+use [% perl.version %];
+use strict;
+[% IF perl.version >= 5.6 %]use warnings;[% END %]
+
 package [% module.name %];
 
+our $VERSION = '[% version %]';
+
 1;
+
+=head1 NAME
+
+[% module.name %] - [% abstract %]
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 AUTHOR
+
+[% author.name %] <[% author.email %]>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) [% date.year %]. [% author.name %].
+
+[% IF license == 'perl' -%]
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+[% END -%]
+
+=cut
+
